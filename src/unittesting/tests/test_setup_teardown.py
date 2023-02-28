0cc39660fb9f939b7f966f6164365946129fbf63
Riley Primeau <primeauriley@gmail.com>
2023-02-28 16:33:47 -0700

    added comments

--- src/unittesting/tests/test_setup_teardown.py
+++ src/unittesting/tests/test_setup_teardown.py
@@ -1,6 +1,10 @@
 import unittest
 
+# PYTEST will be MOST VALUABLE 
+# pytest can be run from the folder and it autmatically finds and executes the tests
+
 class TestValues(unittest.TestCase):
+	# in a different scenario setUps are useful so you dont have to re-delcare the same object
 	def setUp(self) -> None:
 		print("setUp")
 		return super().setUp()
@@ -20,6 +24,11 @@ class TestValues(unittest.TestCase):
 	def test_int_bool(self) -> None:
 		print("test_int_bool")
 		self.assertEqual(1, True)
+
+# You must have a main for calling the test with python...
+# python test_setup_teardown.py
+# ...to work properly.
+# Caling main will go through each test with a report
 
 if __name__ == "__main__":
 	unittest.main(verbosity=2)
